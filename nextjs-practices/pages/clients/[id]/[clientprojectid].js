2fba7f9f16ddad93f413aada130b1f1ce74d0379
praabindh <praabindh@codingmart.com>
2023-06-26 14:36:06 +0530

    Updated NextJS Practices

--- nextjs-practices/pages/clients/[id]/[clientprojectid].js
+++ nextjs-practices/pages/clients/[id]/[clientprojectid].js
@@ -7,7 +7,7 @@ function SelectedClientProjectPage() {
 
   return (
     <div>
-      <h1>The Project Page for a Specific Project for a Selected Client</h1>
+      <h1>The Project Page For a Specific Project For A Selected Client</h1>
     </div>
   );
 }
