c19b94893418e6f8e406a86662fe1d1cdf90b85e
praabindh <praabindh@codingmart.com>
2023-06-26 14:33:49 +0530

    Updated NextJS Practices

--- nextjs-practices/pages/index.js
+++ nextjs-practices/pages/index.js
@@ -1,14 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function HomePage() {
   return (
-    <>
-      <h1>Victory To The Blood Of Jesus</h1>
-    </>
-  )
+    <div>
+      <h1>The Home Page</h1>
+      <ul>
+          <li>
+              <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+              <Link href="/clients">Clients</Link>
+          </li>
+      </ul>
+    </div>
+  );
 }
+
+export default HomePage;
